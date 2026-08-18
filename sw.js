68d88b01633046de6581abb15a6ad7c716589db2
cristianorodrigurssilva-pixel <cristiano.rodrigurssilva@gmail.com>
2026-08-18 16:24:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('app-store').then((cache) => cache.addAll(['index.html'])));
+const CACHE_NAME = 'recibobox-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg' // Certifique-se de que este arquivo existe no GitHub
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
